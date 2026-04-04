e18143c96b3ee1b41b7765f1738ac69fd27fe654
devsrcx <suashish049@outlook.com>
2026-04-04 12:21:46 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,51 +1,59 @@
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.CompletableFuture;
 
 public class Main {
 
     public static void main(String[] args) {
-        // You can use print statements as follows for debugging, they'll be visible when running tests.
-        System.out.println("Logs from your program will appear here!");
-
-        //  Uncomment the code below to pass the first stage
-        ServerSocket serverSocket;
         int port = 6379;
-        try {
-            serverSocket = new ServerSocket(port);
-            // Since the tester restarts your program quite often, setting SO_REUSEADDR
-            // ensures that we don't run into 'Address already in use' errors
+        try (ServerSocket serverSocket = new ServerSocket(port)) {
             serverSocket.setReuseAddress(true);
             while (true) {
+                System.out.println("Waiting for clients on port " + port + "...");
+
                 Socket clientSocket = serverSocket.accept();
-                new Thread(() -> {
-                    try {
-                        InputStream inputStream = clientSocket.getInputStream();
-                        OutputStream outputStream = clientSocket.getOutputStream();
-                        while (true) {
-                            byte[] input = new byte[1024];
-                            int byteCount = inputStream.read(input);
-                            if (byteCount == -1) break;
-                            String inputString = new String(input, 0, byteCount).trim();
-                            System.out.println("Received: " + inputString);
-                            outputStream.write("+PONG\r\n".getBytes());
-                        }
-                    } catch (IOException e) {
-                        System.out.println("Client error: " + e.getMessage());
-                    } finally {
-                        try {
-                            clientSocket.close();
-                        } catch (IOException e) {
-                            System.out.println("Error closing client: " + e.getMessage());
-                        }
-                    }
-                }).start();
+
+                System.out.println("Client connected: " + clientSocket.getInetAddress());
+
+                CompletableFuture.runAsync(() -> {
+                    handleMulptipleClient(clientSocket);
+                });
+
             }
         } catch (IOException e) {
             System.out.println("Server error: " + e.getMessage());
+        }
+    }
+
+    private static void handleMulptipleClient(Socket clientSocket) {
+        try (InputStream inputStream = clientSocket.getInputStream(); OutputStream outputStream = clientSocket.getOutputStream()) {
+
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+
+            String line = bufferedReader.readLine();
+            System.out.println("Received line: " + line);
+            while (line != null)  {
+                System.out.println("Received line: " + line);
+                line = bufferedReader.readLine();
+                
+                if(line.contains("PING")){
+                    outputStream.write("+PONG\r\n".getBytes());
+                }
+            }
+        } catch (IOException e) {
+            System.out.println("Client error: " + e.getMessage());
+        } finally {
+            try {
+                clientSocket.close();
+            } catch (IOException e) {
+                System.out.println("Error closing client socket: " + e.getMessage());
+            }
         }
     }
 }
